7250e2b23c0eb4955b26579fb3bf78ddfd45f422
Максим Багомедов <arsenbagomedovgmail.com>
2023-06-22 16:13:23 +0300

    2primera

--- seminar3/zadacha3/Program.cs
+++ seminar3/zadacha3/Program.cs
@@ -7,3 +7,21 @@ for (int i = 1; i <= number; i++)
 
     Console.Write($"{result} ");
 }
+/*
+double pow_a(int count)
+{
+    double result = Math.Pow(count, 3);
+    return result;
+}
+
+Console.Write("Ведите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= n)
+{
+    double result = pow_a(count);
+    Console.Write($"{result} ");
+    count++;
+}*/
