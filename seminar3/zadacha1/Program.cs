1d69106845099c376897f3ad440f10e576ed674d
Максим Багомедов <arsenbagomedovgmail.com>
2023-06-22 15:31:31 +0300

    DZ1

---
+++ seminar3/zadacha1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. */
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99999 || number < 10000)
+{
+    Console.WriteLine("Вы вели не пятизначное число");
+    Environment.Exit(0);
+}
+string str = number.ToString();
+if(str[0] == str[4]||str[1] == str[3])
+{
+    Console.WriteLine("Поздравляю, введенное слово палиндром");
+}
+else
+{
+    Console.WriteLine("Введенное слово не является полиндромом");
+}
