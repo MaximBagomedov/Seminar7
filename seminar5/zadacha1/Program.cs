e0b9644df8b5fd01ffd5c74a9309a9d979f9d380
Максим Багомедов <arsenbagomedovgmail.com>
2023-06-30 20:38:34 +0300

    DZ1_Seminar5

---
+++ seminar5/zadacha1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+int [] array = new int [4];
+Random random = new Random ();
+int index = 0;
+Console.Write("[");
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = random.Next (100, 1000);
+    Console.Write(array[i] + " ");
+    
+}
+Console.Write("]-> ");
+for (int i = 0; i < array.Length; i++) 
+{
+    if (array[i] % 2 == 0 )
+    {
+        index = index + 1;
+    }
+  
+}
+System.Console.Write(index);
