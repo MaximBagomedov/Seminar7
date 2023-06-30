d97e0ca52992cd13d0ba80f6ff2d75cf012561b7
Максим Багомедов <arsenbagomedovgmail.com>
2023-06-30 20:54:04 +0300

    Dz2_Seminar5

---
+++ seminar5/zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+int [] array = new int [4];
+Random random = new Random ();
+int index = 0;
+Console.Write("[");
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = random.Next (0, 100);
+    Console.Write(array[i] + " ");
+}
+Console.Write("]-> ");
+for (int i = 0; i < array.Length; i++) 
+{
+    if (array[i] % 2 != 0)
+    {
+        index = index + 1;
+    }
+}
+Console.Write(index);
