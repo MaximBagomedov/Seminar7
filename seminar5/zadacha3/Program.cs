4c84b46cef433523e715508599501f2d2c0e5d12
Максим Багомедов <arsenbagomedovgmail.com>
2023-06-30 21:54:51 +0300

    Dz3_seminar5

---
+++ seminar5/zadacha3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76*/
+double[] array = {3.22, 4.2, 1.15, 77.15, 65.2};
+Random random = new Random();
+Console.Write("[");
+for (int i = 0; i <array.Length; i++ )
+{
+    Console.Write(array[i] + " ");
+}
+Console.Write("]-> ");
+double max = 0;
+double min = array[0];
+for (int i = 0; i <array.Length; i++ )
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+double result = max - min;
+System.Console.WriteLine($"{max} - {min} = {result}");
