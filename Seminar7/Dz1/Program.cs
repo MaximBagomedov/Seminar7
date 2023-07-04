2dec976d17c91358eea9fb4d969395f65395776c
Максим Багомедов <arsenbagomedovgmail.com>
2023-07-04 15:28:56 +0300

    Dz1_seminar7

---
+++ Seminar7/Dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Write("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбец в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m , n];
+
+Random random = new Random();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <= m; j++)
+    {
+        array[i, j] = Math.Round(random.NextDouble(), 2);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
